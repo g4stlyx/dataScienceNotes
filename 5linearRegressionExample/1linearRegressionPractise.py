3a7c8a553cc83948b2c786d5993158572a911b2e
g4stlyx <g4stlyonline@gmail.com>
2024-09-25 15:30:11 +0300

    linear regression real life example

--- 5linearRegressionExample/1linearRegressionPractise.py
+++ 5linearRegressionExample/1linearRegressionPractise.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_theme()
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 #! the aim is predicting the price of a used car depending on its specifications
@@ -63,7 +65,7 @@ print(data_cleaned.describe(include="all"))
 # plt.show()
 
 #! Checking the OLS assumptions (part 2)
-
+"""
 f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, figsize =(15,3))
 ax1.scatter(data_cleaned['Year'],data_cleaned['Price'])
 ax1.set_title('Price and Year')
@@ -71,6 +73,7 @@ ax2.scatter(data_cleaned['EngineV'],data_cleaned['Price'])
 ax2.set_title('Price and EngineV')
 ax3.scatter(data_cleaned['Mileage'],data_cleaned['Price'])
 ax3.set_title('Price and Mileage')
+"""
 # plt.show()
 
 # we spot some patterns, but not linear ones. so we should first transform one or more variables
@@ -80,7 +83,7 @@ log_price = np.log(data_cleaned['Price'])
 data_cleaned['log_price'] = log_price
 
 # plot them with the log_price
-
+"""
 f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, figsize =(15,3))
 ax1.scatter(data_cleaned['Year'],data_cleaned['log_price'])
 ax1.set_title('Log Price and Year')
@@ -88,6 +91,7 @@ ax2.scatter(data_cleaned['EngineV'],data_cleaned['log_price'])
 ax2.set_title('Log Price and EngineV')
 ax3.scatter(data_cleaned['Mileage'],data_cleaned['log_price'])
 ax3.set_title('Log Price and Mileage')
+"""
 # plt.show()
 
 data_cleaned = data_cleaned.drop(['Price'], axis=1) # since log_price works better for linearity, we dont need Price anymore
@@ -109,12 +113,84 @@ data_no_multicollinearity = data_cleaned.drop(['Year'], axis=1)
 print(data_no_multicollinearity)
 
 #! create dummy variables (part 3)
+# if we have n categories for a feature, we have to create n-1 dummies
 
+data_with_dummies = pd.get_dummies(data_no_multicollinearity, drop_first=True)
 
+print(data_with_dummies)
 
+cols = ['log_price', 'Mileage', 'EngineV', 'Brand_BMW',
+       'Brand_Mercedes-Benz', 'Brand_Mitsubishi', 'Brand_Renault',
+       'Brand_Toyota', 'Brand_Volkswagen', 'Body_hatch', 'Body_other',
+       'Body_sedan', 'Body_vagon', 'Body_van', 'Engine Type_Gas',
+       'Engine Type_Other', 'Engine Type_Petrol', 'Registration_yes']
 
+data_preprocessed = data_with_dummies[cols]
+print(data_preprocessed.head())
 
+#! linear regression model
+#* inputs and targets
+targets = data_preprocessed['log_price']
+inputs = data_preprocessed.drop(['log_price'], axis=1)
 
+#* standardization
+scaler = StandardScaler()
+scaler.fit(inputs)
+inputs_scaled = scaler.transform(inputs)    # we normally shouldnt standardize dummy variables
 
+#* train test split
 
+x_train, x_test, y_train, y_test = train_test_split(inputs_scaled, targets, test_size=0.2, random_state=365)
 
+#* creating the regression
+
+reg= LinearRegression()
+reg.fit(x_train, y_train)
+
+y_hat = reg.predict(x_train)
+# plt.scatter(y_train, y_hat)
+# plt.xlabel('Targets (y_train)', size=18)
+# plt.ylabel('Predictions (y_hat)', size=18)
+# plt.xlim(6,13)
+# plt.ylim(6,13)
+# plt.show()
+
+# sns.histplot(y_train - y_hat, kde=True, stat="density", linewidth=0)
+# plt.title("Residuals PDF", size=18) # standard normal distribution with a long tail on left. mean = 0
+# plt.show()
+
+reg.score(x_train,y_train) # 0.74, meaning our model is explaining 74% of variablity of the data
+
+reg_summary = pd.DataFrame(inputs.columns.values, columns=['Features'])
+reg_summary['Weights'] = reg.coef_
+print("*****************")
+print(reg_summary)
+
+# positive weight shows that as a feature increases in value, so do the log_price and 'Price'. kısaca weight pozitif = o özellikle tahmin edilen özellik arasında doğru orantı var.
+# negative weight = ters orantı
+
+# for dummy variables, we had n-1 dummies. that -1 is caused by the BENCHMARK VALUE, in our case the brand 'Audi'
+# so if the weight of a dummy variable is positive, its more expansive than 'Audi'.
+
+#! Testing
+
+y_hat_test = reg.predict(x_test)
+
+plt.scatter(y_test, y_hat_test, alpha=0.2) # the more saturated the color, the higher the concentration
+plt.xlabel('Targets (y_test)', size=18)
+plt.ylabel('Predictions (y_hat_test)', size=18)
+plt.xlim(6,13)
+plt.ylim(6,13)
+plt.show()
+
+df_pf = pd.DataFrame(np.exp(y_hat_test), columns=['Predictions']) # data frame performance
+
+y_test = y_test.reset_index(drop=True)
+df_pf['Target'] = np.exp(y_test)
+df_pf['Residuals'] = df_pf['Target'] - df_pf['Predictions']
+df_pf['Difference%'] = np.absolute(df_pf['Residuals']/df_pf['Target']*100)
+
+print("*********")
+print(df_pf.describe())
+print("**************")
+print(df_pf.sort_values(by=['Difference%'])) # most of the predictions are pretty good.
